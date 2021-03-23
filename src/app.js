f08b32f7a747b6dd47eef6ea8984350780452e9f
Bryan-Murl <murillo.bryan@outlook.com>
2021-03-23 20:08:31 +0000

    Finish Domain generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "rave"];
+
+  function GenerateDomain() {
+    pronoun.forEach(element1 => {
+      adj.forEach(element2 => {
+        noun.forEach(element3 => {
+          console.log(element1.concat(element2.concat(element3, ".com")));
+        });
+      });
+    });
+  }
+
+  GenerateDomain();
 };
